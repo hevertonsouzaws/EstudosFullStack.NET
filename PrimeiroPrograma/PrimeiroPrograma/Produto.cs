62d24943a222281886631b0339bd55cdfc2df668
HEVERTON NASCIMENTO SOUZA <heverton.souza@websupply.com.br>
2025-08-20 11:13:01 -0300

    Estudos WS

---
+++ PrimeiroPrograma/PrimeiroPrograma/Produto.cs
@@ -0,0 +1,46 @@
+﻿namespace PrimeiroPrograma
+{
+    internal class Produto
+    {
+        public string Descrição;
+        public double Preco;
+        public int Quantidade;
+
+        /* Construtor
+        public Produto(string nome, double preco, int quantidade)
+        {
+            nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        } */
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+
+        // Método para exibir as informações do produto 
+        public override string ToString()
+        {
+            return Descrição 
+                + "\n | preço: R$ " 
+                + Preco.ToString("F2")
+                + "\n | estoque: "
+                + Quantidade
+                + "\n | valor total: R$ "
+                + ValorTotalEmEstoque();
+        }
+      
+    }
+}
