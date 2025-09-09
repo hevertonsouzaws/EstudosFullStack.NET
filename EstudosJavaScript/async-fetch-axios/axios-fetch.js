dbeb51c12ca3b373800f408786793338465fc971
HEVERTON NASCIMENTO SOUZA <heverton.souza@websupply.com.br>
2025-09-09 11:05:34 -0300

    feat: códigos para estudo

---
+++ EstudosJavaScript/async-fetch-axios/axios-fetch.js
@@ -0,0 +1,55 @@
+// 1 - get fetch
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Retorno do FETCH:')
+        console.log(data)
+    }).catch((error) => {
+        console.log(error)
+    })
+
+
+// 2 - get com axios
+axios
+    .get("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => {
+        console.log('Retorno do AXIOS')
+        console.log(response.data)
+    }).catch((error) => {
+        console.log(error);
+    })
+
+// 3 - POST com fetch
+
+const data = {
+    title: "#SomosWS",
+    content: "POST com fetch",
+    userId: 1
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json" // tipo de contéudo 
+    },
+    body: JSON.stringify(data),  //converte para Json    
+})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Retorno POST do FETCH:')
+        console.log(data)
+    }).catch((error) => {
+        console.log(error)
+    })
+
+
+// 4 - POST com axios
+
+axios.post('https://jsonplaceholder.typicode.com/posts', data)
+    .then((response) => {
+        console.log('Retorno POST do AXIOS')
+        console.log(response.data)
+    }).catch((error) => {
+        console.log(error);
+    })
