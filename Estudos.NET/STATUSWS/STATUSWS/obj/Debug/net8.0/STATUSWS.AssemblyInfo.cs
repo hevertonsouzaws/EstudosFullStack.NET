1953dfea762793aea0827804fee65c0eea6f636f
HEVERTON NASCIMENTO SOUZA <heverton.souza@websupply.com.br>
2025-09-24 14:18:58 -0300

    fix: ajuste Dtos e Controllers

--- Estudos.NET/STATUSWS/STATUSWS/obj/Debug/net8.0/STATUSWS.AssemblyInfo.cs
+++ Estudos.NET/STATUSWS/STATUSWS/obj/Debug/net8.0/STATUSWS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StatusWS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c26bf9764a2cbfb0b80744bcd17da92c5bc819")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+497c4ad236989d31c080314b23670fe49897cb11")]
 [assembly: System.Reflection.AssemblyProductAttribute("StatusWS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StatusWS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
